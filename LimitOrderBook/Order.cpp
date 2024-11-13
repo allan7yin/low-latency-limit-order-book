5f4bac5a7317b830cb68341db4ecd19ae087a9b8
Allan Yin <a7yin@uwaterloo.ca>
2024-11-13 16:51:12 -0500

    New changes

--- LimitOrderBook/Order.cpp
+++ LimitOrderBook/Order.cpp
@@ -1,53 +1,35 @@
 #include "Order.hpp"
+#include "Limit.hpp"
 
-int Order::getId() const {
-    return id;
-}
+int Order::getId() const { return id; }
 
-bool Order::getBuyOrSell() const {
-    return buyOrSell;
-}
+bool Order::getBuyOrSell() const { return buyOrSell; }
 
-int Order::getShares() const {
-    return shares;
-}
+int Order::getShares() const { return shares; }
 
-int Order::getLimit() const {
-    return limit;
-}
+int Order::getLimit() const { return limit; }
 
-int Order::getEntryTime() const {
-    return entryTime;
-}
+int Order::getEntryTime() const { return entryTime; }
 
-int Order::getEventTime() const {
-    return eventTime;
-}
+int Order::getEventTime() const { return eventTime; }
 
-Limit *Order::getParentLimit() const {
-    return parentLimit;
-}
+Limit *Order::getParentLimit() const { return parentLimit; }
 
 void Order::partiallyFillOrder(int orderShares) {
-    shares -= orderShares;
-    parentLimit->partiallyFillVolume(orderShares);
+  shares -= orderShares;
+  parentLimit->partiallyFillVolume(orderShares);
 }
 
 std::ostream &operator<<(std::ostream &os, const Order &order) {
-    os << "Order ID: " << order.id << ", "
-       << "Shares: " << order.shares << ", "
-       << "Limit: " << order.limit << ", "
-       << "Entry Time: " << order.entryTime << ", "
-       << "Event Time: " << order.eventTime;
-    return os;
+  os << "Order ID: " << order.id << ", "
+     << "Shares: " << order.shares << ", "
+     << "Limit: " << order.limit << ", "
+     << "Entry Time: " << order.entryTime << ", "
+     << "Event Time: " << order.eventTime;
+  return os;
 }
 
-Order::Order(int id, bool buyOrSell, int shares, int limit, int entryTime, int eventTime)
-    : id(id),
-      buyOrSell(buyOrSell),
-      shares(shares),
-      limit(limit),
-      entryTime(entryTime),
-      eventTime(eventTime),
-      parentLimit(nullptr) {
-}
+Order::Order(int id, bool buyOrSell, int shares, int limit, int entryTime,
+             int eventTime)
+    : id(id), buyOrSell(buyOrSell), shares(shares), limit(limit),
+      entryTime(entryTime), eventTime(eventTime), parentLimit(nullptr) {}
--- LimitOrderBook/Order.hpp
+++ LimitOrderBook/Order.hpp
@@ -1,33 +1,33 @@
 #ifndef order_hpp
 #define order_hpp
 
-#include "Limit.hpp"
 #include <iostream>
 
 class Limit;
 
 class Order {
-  private:
-    int id;
-    bool buyOrSell;
-    int shares;
-    int limit;
-    int entryTime;
-    int eventTime;
-    Limit *parentLimit;
+private:
+  int id;
+  bool buyOrSell;
+  int shares;
+  int limit;
+  int entryTime;
+  int eventTime;
+  Limit *parentLimit;
 
-  public:
-    Order(int id, bool buyOrSell, int shares, int limit, int entryTime = 0, int eventTime = 0);
-    int getId() const;
-    bool getBuyOrSell() const;
-    int getShares() const;
-    int getLimit() const;
-    int getEntryTime() const;
-    int getEventTime() const;
-    Limit *getParentLimit() const;
-    void partiallyFillOrder(int orderShares);
+public:
+  Order(int id, bool buyOrSell, int shares, int limit, int entryTime = 0,
+        int eventTime = 0);
+  int getId() const;
+  bool getBuyOrSell() const;
+  int getShares() const;
+  int getLimit() const;
+  int getEntryTime() const;
+  int getEventTime() const;
+  Limit *getParentLimit() const;
+  void partiallyFillOrder(int orderShares);
 
-    friend std::ostream &operator<<(std::ostream &os, const Order &order);
+  friend std::ostream &operator<<(std::ostream &os, const Order &order);
 };
 
 #endif
